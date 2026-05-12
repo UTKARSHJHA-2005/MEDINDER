d717ddb44ad7fbe4a81923e0594027f10c4e1422
Utkarsh Jha <jha.utkarsh2005@gmail.com>
2026-05-12 16:02:24 +0530

    Capsule

--- app/login/login.tsx
+++ app/login/login.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -6,79 +6,113 @@ import {
   Alert,
   TouchableOpacity,
   TextInput,
-} from 'react-native';
-import { Auth } from '../config/db';
-import { useAuthRequest, makeRedirectUri } from 'expo-auth-session';
-import { getApp } from 'firebase/app';
-import { setstorage } from '../service/Storage';
-import { signInWithEmailAndPassword, getAuth, GoogleAuthProvider, signInWithCredential } from 'firebase/auth';
-import { Entypo } from '@expo/vector-icons';
-import auth from '@react-native-firebase/auth';
-import * as WebBrowser from 'expo-web-browser';
-import * as AuthSession from 'expo-auth-session';
-import * as LocalAuthentication from 'expo-local-authentication'
+} from "react-native";
+import { Auth } from "../config/db";
+import { useAuthRequest, makeRedirectUri } from "expo-auth-session";
+import { getApp } from "firebase/app";
+import { setstorage } from "../service/Storage";
+import {
+  signInWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  signInWithCredential,
+} from "firebase/auth";
+import { Entypo } from "@expo/vector-icons";
+import auth from "@react-native-firebase/auth";
+import * as WebBrowser from "expo-web-browser";
+import * as AuthSession from "expo-auth-session";
+import * as LocalAuthentication from "expo-local-authentication";
 // import auth from '@react-native-firebase/auth';
-import { StatusBar } from 'expo-status-bar';
-import { useRouter } from 'expo-router';
-import { GoogleSignin, isSuccessResponse, isErrorWithCode, statusCodes } from '@react-native-google-signin/google-signin';
+import { StatusBar } from "expo-status-bar";
+import { useRouter } from "expo-router";
+import {
+  GoogleSignin,
+  isSuccessResponse,
+  isErrorWithCode,
+  statusCodes,
+} from "@react-native-google-signin/google-signin";
 
 GoogleSignin.configure({
-  webClientId: "70506501293-b2pi75ees1234u0bb6ls9iac3g7iuoej.apps.googleusercontent.com",
-})
+  webClientId:
+    "70506501293-b2pi75ees1234u0bb6ls9iac3g7iuoej.apps.googleusercontent.com",
+});
 const Login = () => {
   // const app = getApp();
   // const auth = getAuth(app);
   const router = useRouter();
   const [isBiometric, setIsBiometric] = useState(false);
-  const [username, setUsername] = useState('');
-  const route = useRouter()
+  const [username, setUsername] = useState("");
+  const route = useRouter();
   const [userinfo, setuserinfo] = useState<any>(null);
-  const [password, setPassword] = useState('');
-  const CLIENT_ID = "521789138294-lg66qlkesrnevf1crb3n8a9sbfaihq1v.apps.googleusercontent.com";
+  const [password, setPassword] = useState("");
+  const CLIENT_ID =
+    "521789138294-lg66qlkesrnevf1crb3n8a9sbfaihq1v.apps.googleusercontent.com";
 
   const handleBiometric = async () => {
     const isAvailable = await LocalAuthentication.hasHardwareAsync();
     const savedBiometric = await LocalAuthentication.isEnrolledAsync();
     if (!isAvailable || !savedBiometric) {
-      Alert.alert('Biometric login not available or set up');
+      Alert.alert("Biometric login not available or set up");
       return;
     }
     const biometricAuth = await LocalAuthentication.authenticateAsync({
-      promptMessage: 'Login with Biometrics',
-      cancelLabel: 'Cancel',
+      promptMessage: "Login with Biometrics",
+      cancelLabel: "Cancel",
       disableDeviceFallback: true,
     });
     if (biometricAuth.success) {
-      const userCredential = await signInWithEmailAndPassword(Auth, username, password);
-      await setstorage('userDetail', userCredential.user);
+      const userCredential = await signInWithEmailAndPassword(
+        Auth,
+        username,
+        password,
+      );
+      await setstorage("userDetail", userCredential.user);
       console.log("Logged in successfully:", userCredential.user);
-      router.replace('/(tabs)');
+      router.replace("/(tabs)");
     } else {
-      Alert.alert('Biometric login failed');
+      Alert.alert("Biometric login failed");
     }
   };
 
   const handleLogin = async () => {
     try {
-      const userCredential = await signInWithEmailAndPassword(Auth, username, password);
-      router.replace('/(tabs)');
-      await setstorage('userDetail', userCredential.user);
+      const userCredential = await signInWithEmailAndPassword(
+        Auth,
+        username,
+        password,
+      );
+      router.replace("/(tabs)");
+      await setstorage("userDetail", userCredential.user);
       console.log("Logged in successfully:", userCredential.user);
     } catch (error) {
       console.error("Error logging in:", error);
     }
-  }
+  };
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Login</Text>
-      <TextInput placeholder="Username" style={styles.input} value={username} onChangeText={setUsername} autoCapitalize="none" />
-      <TextInput placeholder="Password" style={styles.input} value={password} onChangeText={setPassword} secureTextEntry />
+      <TextInput
+        placeholder="Username"
+        style={styles.input}
+        placeholderTextColor="#111010"
+        value={username}
+        onChangeText={setUsername}
+        autoCapitalize="none"
+      />
+      <TextInput
+        placeholder="Password"
+        style={styles.input}
+        placeholderTextColor="#171616"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
       <TouchableOpacity style={styles.loginBtn} onPress={handleLogin}>
         <Text style={styles.loginText}>Login</Text>
       </TouchableOpacity>
       <View style={styles.linkRow}>
-        <TouchableOpacity onPress={() => router.push('/signup/signup')}>
+        <TouchableOpacity onPress={() => router.push("/signup/signup")}>
           <Text style={styles.link}>Create your account</Text>
         </TouchableOpacity>
         <TouchableOpacity>
@@ -100,56 +134,56 @@ export default Login;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    justifyContent: "center",
     paddingHorizontal: 20,
   },
   title: {
     fontSize: 28,
-    fontWeight: 'bold',
-    textAlign: 'center',
+    fontWeight: "bold",
+    textAlign: "center",
     marginBottom: 40,
   },
   input: {
     height: 50,
-    borderColor: '#ccc',
+    borderColor: "#ccc",
     borderWidth: 1,
-    color: 'black',
+    color: "black",
     borderRadius: 10,
     paddingHorizontal: 10,
     marginBottom: 15,
   },
   loginBtn: {
-    backgroundColor: 'black',
+    backgroundColor: "black",
     padding: 15,
     borderRadius: 10,
-    alignItems: 'center',
+    alignItems: "center",
     marginBottom: 10,
   },
   loginText: {
-    color: 'white',
-    fontWeight: 'bold',
+    color: "white",
+    fontWeight: "bold",
   },
   googleBtn: {
-    backgroundColor: '#4285F4',
+    backgroundColor: "#4285F4",
     padding: 15,
     borderRadius: 10,
-    alignItems: 'center',
+    alignItems: "center",
   },
   googleText: {
-    color: 'white',
-    fontWeight: 'bold',
+    color: "white",
+    fontWeight: "bold",
   },
   linkRow: {
     marginTop: 15,
-    alignItems: 'center',
+    alignItems: "center",
   },
   link: {
-    color: '#007AFF',
+    color: "#007AFF",
     marginTop: 8,
   },
   fingerprint: {
     marginTop: 40,
-    alignItems: 'center',
+    alignItems: "center",
   },
 });
