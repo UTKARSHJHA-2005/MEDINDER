d155bbc84c3e1f4867cc30f8645a27823e36874d
Utkarsh Jha <jha.utkarsh2005@gmail.com>
2026-05-15 13:01:11 +0530

    Servicenoti

---
+++ app/service/Notification.tsx
@@ -0,0 +1,33 @@
+import * as Notifications from "expo-notifications";
+
+export async function scheduleMedicineNotification(med: any) {
+  const [hour, minute] = med.reminder.split(":").map(Number);
+
+  let currentDate = new Date(med.startDate);
+  const endDate = new Date(med.endDate);
+
+  currentDate.setHours(0, 0, 0, 0);
+
+  while (currentDate <= endDate) {
+    const trigger = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth(),
+      currentDate.getDate(),
+      hour,
+      minute,
+    );
+
+    if (trigger > new Date()) {
+      await Notifications.scheduleNotificationAsync({
+        content: {
+          title: "💊 Medicine Reminder",
+          body: `Time to take ${med.medName}`,
+          sound: true,
+        },
+        trigger: { date: trigger },
+      });
+    }
+
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
+}
